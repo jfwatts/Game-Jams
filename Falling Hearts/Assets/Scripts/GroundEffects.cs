a85fa04afc91eccf77be29d47460084a04ef466d
Jeremiah Watts <jfwatts@users.noreply.github.com>
2016-01-24 01:07:30 -0600

    Effects for collision
    This isn’t used yet

---
+++ Falling Hearts/Assets/Scripts/GroundEffects.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundEffects : MonoBehaviour {
+
+	void OnParticleCollision(GameObject other){
+		// Pause really quickly
+		//Time.timeScale = 0.1f;
+
+
+	}
+
+	IEnumerator ScreenShake(){
+		yield return new WaitForSeconds (0.01f);
+		Time.timeScale = 1;
+	}
+}
