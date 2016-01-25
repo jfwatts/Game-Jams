da1752411bff20cb9b12270ea0e1efe90045b5fe
Jeremiah Watts <jfwatts@users.noreply.github.com>
2016-01-24 19:51:49 -0600

    Handled mouse movement making it work like a real screensaver

--- Falling Hearts/Assets/Scripts/ScreensaverBehavior.cs
+++ Falling Hearts/Assets/Scripts/ScreensaverBehavior.cs
@@ -3,9 +3,24 @@ using System.Collections;
 
 public class ScreensaverBehavior : MonoBehaviour {
 
+	float startTime;
+	Vector3 lastMousePos;
+
+	void Awake(){
+		startTime = Time.time;
+		lastMousePos = Input.mousePosition;
+		Cursor.visible = false;
+	}
+
+
 	// Update is called once per frame
 	void Update () {
-		if (Input.anyKeyDown)
-			Application.Quit();
+		if (Time.time - startTime > 3) {
+			if (Input.anyKeyDown || (int)lastMousePos.x != (int)Input.mousePosition.x || (int)lastMousePos.y != (int)Input.mousePosition.y) {
+				Application.Quit ();
+			}
+		}
+
+		lastMousePos = Input.mousePosition;
 	}
 }
